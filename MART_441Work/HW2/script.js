31fa39c45459795dbc57d0c7bac3bfbf7a4731d4
alexisehlers <70082130+alexisehlers@users.noreply.github.com>
2022-01-31 20:48:06 -0700

    comitting

---
+++ MART_441Work/HW2/script.js
@@ -0,0 +1,13 @@
+/*
+Author: Alexis Ehlers
+File Name: index.html
+
+Date: 1/31/2022
+Purpose: Homework 2
+*/
+
+
+console.log("1. My favorite websites are Pinterest, Instagram, and Etsy.");
+console.log("2. My favorite games to play growing up were probably Mario KArt, Wii, and Runescape.");
+console.log("3. My favorite artists include Van Gogh, Ed Sheeran, and local photographer Chance Jackson.");
+console.log("The cultural importance of these different artists is as follows: Van Gogh made a big impression on me because of the vibrant colors and strokes that characterize his work. This fits my style and inspires my work as well. In music Ed Sheeran is extremely influential, but as a songwriter I feel that he speaks certain truths that are valid to this generation. Chance Jackson is a photographer located in the Flathead Valley and really captures the beauty and wildness of the region. His photographs create a calming and reverent effect.");
--- README.md
+++
@@ -1,5 +0,0 @@
-# MART441-WebTechnologies
-Homework Repository
-
-Base URL for site:
-https://alexisehlers.github.io/MART441-WebTechnologies/
