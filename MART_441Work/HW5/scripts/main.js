610aa4d6f5dfc7aa891a108675bfb684526d3d31
alexisehlers <70082130+alexisehlers@users.noreply.github.com>
2022-02-22 21:51:09 -0700

    comitting

---
+++ MART_441Work/HW5/scripts/main.js
@@ -0,0 +1,42 @@
+var imageTags = ["image1", "image2", "image3", "image4"];
+var blankImagePath = "./images/dollar.jpg";
+var actualImages = new Array();
+
+function printBlanks()
+{
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+
+
+
+}
+
+function createRandomImageArray()
+{
+    var actualImagePath = ["./images/duck.jpg", "./images/flower.jpg", "./images/dollar.jpg",];
+    var count = [0,0];
+    while(actualImages.length < 4)
+    {
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+        if(count[randomNumber] < 2)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+
+
+
+
+
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];
+
+
+}
